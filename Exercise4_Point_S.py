b4094dfc2082c5f023fc40602ca7215aca4aa33f
PointSuperDuck <PointSuperDuck@users.noreply.github.com>
2020-08-13 14:14:58 +0700

    add score

---
+++ Exercise4_Point_S.py
@@ -0,0 +1,7 @@
+FE = "Foundation English"
+GB = "General Business"
+ICS = "Introduction to Computer Systems"
+CP = "Computer Programming"
+
+print(FE," = ",80,GB," = ",75,ICS," = ","50",CP," = ",45)
+
